ed3527a06b8e8ee87dfb08b769e20a06897c4491
Islam Hany <ihhg.developer@gmail.com>
2023-12-03 06:54:38 +0200

    Change CustomUser to use default id field instead of UUIDField

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,16 +3,10 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now
-from uuid import uuid4
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField( 
-        _('id'),
-        primary_key = True, 
-        default = uuid4, 
-        editable = False) 
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
