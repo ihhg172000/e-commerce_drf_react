2e80c1bf796f446cb15fb7cc3283eaa128978fae
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-12-09 22:36:29 +0200

    init models of Product app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
+	path("product/", include("product.urls"))
 ]
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from users.models import User
+"""category table"""
+class category(models.TextChoices):
+	Mobiles = "Mobiles"
+	Accessories = "Accessories"
 
-# Create your models here.
+
+"""product table"""
+class Product(models.Model):
+	name = models.CharField(max_length=200, default="", blank=False)
+	description = models.TextField(max_length=1000, default="", blank=False)
+	price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+	quantity = models.IntegerField(default=0)
+	#brand_id = 
+	#category_id = 
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+
+	
+
+
