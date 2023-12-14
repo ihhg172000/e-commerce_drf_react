61753e95aa4f7c8240e11b4293245de6c4ddbf94
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-12-14 21:29:09 +0200

    product database models main features

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from product.models import Product
+from product.models import Product,Category, Brand
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Brand)
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 from users.models import User
+
+
+
 """category table"""
-class category(models.TextChoices):
-	Mobiles = "Mobiles"
-	Accessories = "Accessories"
+class Category(models.Model):
+	name = models.CharField(max_length=200, default="", blank=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return (self.name)
+
+
+
+"""Brand table"""
+class Brand(models.Model):
+	name = models.CharField(max_length=200, default="", blank=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return (self.name)
+
 
 
 """product table"""
@@ -12,11 +31,15 @@ class Product(models.Model):
 	description = models.TextField(max_length=1000, default="", blank=False)
 	price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
 	quantity = models.IntegerField(default=0)
-	#brand_id = 
-	#category_id = 
+	#image = 
+	brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)        # many to one 
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) # many to one
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return (self.name)
 
 
 
